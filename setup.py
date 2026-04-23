cd9a83cb084fae3927be79b82be6a53049e3814f
Katherine Laliotis <67384941+kklaliotis@users.noreply.github.com>
2026-04-23 10:54:18 -0400

    Rename package from 'templatecode' to 'jwst-destripe-driver'

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-        name="templatecode",
+        name="jwst-destripe-driver",
         version="0.0",
         packages=find_packages(),
         install_requires=[
